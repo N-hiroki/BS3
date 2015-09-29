fbd801c4c5099c7927c7f37199a2e45f60f0eedb
中島弘貴 <zero.flowerbox.isk76@gmail.com>
2015-09-30 03:00:07 +0900

    aタグ変更

--- history_list.php
+++ history_list.php
@@ -10,8 +10,8 @@
     $id = htmlspecialchars($_GET["id"]);
     $pdo = new PDO('mysql:dbname=bs;host=localhost', 'root', '');
     $stmt = $pdo->query('SET NAMES utf8');
-    $stmt = $pdo->prepare("SELECT * FROM question WHERE user_id=$id ORDER BY date DESC LIMIT 5");
-    $stmt = $pdo->prepare("SELECT * FROM question WHERE user_id=qwe ORDER BY date DESC LIMIT 5");
+    $stmt = $pdo->prepare("SELECT * FROM question WHERE user_id='$id' ORDER BY date DESC LIMIT 5");
+    //$stmt = $pdo->prepare("SELECT * FROM question WHERE user_id = 'dhdhdjhi'");
     $flag = $stmt->execute();
     if($flag==false){
         $view = "SQLエラー";
@@ -19,7 +19,7 @@
         $view .= '<br>';
         while( $result = $stmt->fetch(PDO::FETCH_ASSOC)){
         //新規質問
-        $view .= '<a href="question_history.php?        id='.$result['id'].'&id='.$id.'"method="get"action="question_history.php">'.$result['title'].$result['date'].'</a><br>';
+        $view .= '<a href="question_history.php?        question_id='.$result['id'].'&id='.$id.'"method="get"action="question_history.php">「'.$result['title'].'」'.$result['date'].'</a><br>';
         }
     }
 ?>
@@ -33,6 +33,5 @@
 <form class="form-inline button_position" method="post" action="">
     <input type="button" class="form-control btn-primary width100" value="前へ">
     <input type="button" class="form-control btn-primary width100 margin_left50" value="次へ">
-    <input type="hidden" name="post_flg" value="1">
 </form>
 <?php require('require/footer.php'); ?>
