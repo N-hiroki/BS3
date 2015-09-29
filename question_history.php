3d0b800157ed7a2b008c070c53c19574b710e955
中島弘貴 <zero.flowerbox.isk76@gmail.com>
2015-09-30 03:01:56 +0900

    aタグ変更

--- question_history.php
+++ question_history.php
@@ -12,6 +12,7 @@ question_IDをキーに検索
     $stmt_ans = $pdo->query('SET NAMES utf8');
 //    question id 取得
     $id = htmlspecialchars($_GET["id"]);
+    $question_id = htmlspecialchars($_GET["question_id"]);
 //    質問検索
     $stmt = $pdo->prepare("SELECT * FROM question WHERE user_id='$id'");
     $flag = $stmt->execute();
@@ -22,7 +23,6 @@ question_IDをキーに検索
     }else{
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
         $view .= '<br>ユーザー:'.$result['user_id'].'<br>作成日:'.$result['date'].'<br><hr>'.$result['title'].'<br><hr>'.$result['question'].'<br><hr>';
-        $user_id = $result['user_id'];
     }
 //    回答検索
     $stmt_ans = $pdo->prepare("SELECT * FROM ans WHERE id='$id'");
@@ -45,5 +45,5 @@ question_IDをキーに検索
     <?=$ans?>
 </div>
 <a href="history_list.php?id=$user_id"><input type="button" class="form-control btn-primary width100 button_position" value="戻る"></a>
-<a href="update.php?id=$id"><input type="button" class="form-control btn-primary width100 button_position float_left" value="修正"></a>
+<a href="update.php?id=<?=$id?>&question_id=<?=$question_id?>"><input type="button" class="form-control btn-primary width100 button_position float_left" value="修正"></a>
 <?php require('require/footer.php'); ?>
