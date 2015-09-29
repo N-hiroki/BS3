4e05e28195429a0edb7bf8c26628c6d4f40756b5
中島弘貴 <zero.flowerbox.isk76@gmail.com>
2015-09-30 03:04:12 +0900

    修正

--- update_execute.php
+++ update_execute.php
@@ -1,18 +1,18 @@
 <?php
     $id = $_POST["id"];
+    $question_id = $_POST["question_id"];
     $title = htmlspecialchars($_POST["title"]);
     $question = htmlspecialchars($_POST["question"]);
-    $user_id = $_POST["user_id"];
     $pdo = new PDO('mysql:dbname=bs;host=localhost', 'root', '');
     $stmt = $pdo->query('SET NAMES utf8');
     $stmt = $pdo->prepare("UPDATE question SET title=?, question=? WHERE id=?");
-    $stmt_update->bindParam(1,$title, PDO::PARAM_STR);
-    $stmt_update->bindParam(2,$question, PDO::PARAM_STR);
-    $stmt_update->bindParam(3,$id, PDO::PARAM_INT);
-    $status = $stmt_update->execute();   //sql実行
+    $stmt->bindParam(1,$title, PDO::PARAM_STR);
+    $stmt->bindParam(2,$question, PDO::PARAM_STR);
+    $stmt->bindParam(3,$question_id, PDO::PARAM_INT);
+    $status = $stmt->execute();   //sql実行
     if($status==false){
         echo "SQLエラー";
     }else{
-        header("Location: index.php?id=$user_id");
+        header("Location: index.php?id='$id'");
     }
 ?>
