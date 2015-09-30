2e4e23b0019084b7b4318459ceba952d8daa8bad
中島弘貴 <zero.flowerbox.isk76@gmail.com>
2015-09-30 13:17:48 +0900

    sessionをrequireで呼び出しに変更

--- question_execute.php
+++ question_execute.php
@@ -1,16 +1,16 @@
 <?php
-    $user_id = $_POST["id"];
+    require('require/session.php');
     $title = $_POST["title"];
     $question = $_POST["question"];
     $date = date("Y-m-d H:i:s");
     
     $pdo = new PDO('mysql:dbname=bs;host=localhost', 'root', '');
     $stmt = $pdo->query('SET NAMES utf8');
-    $stmt = $pdo->prepare("INSERT INTO question(id,title,question,date,user_id)VALUES(null,:title,:question,:date,:user_id)");
+    $stmt = $pdo->prepare("INSERT INTO question(id,title,question,date,user_id)VALUES(null,:title,:question,:date,:id)");
     $stmt->bindValue(':title', $title);
     $stmt->bindValue(':question', $question);
     $stmt->bindValue(':date', $date);
-    $stmt->bindValue(':user_id', $user_id);
+    $stmt->bindValue(':id', $id);
     $status = $stmt->execute();   //sql実行
     if($status==false){
         echo "SQLエラー";
