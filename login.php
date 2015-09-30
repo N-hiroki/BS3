bc2bfeeeccec0ac0409cbaf3d2f0611abbbf714c
中島弘貴 <zero.flowerbox.isk76@gmail.com>
2015-09-30 12:37:44 +0900

    sessionの追加

--- login.php
+++ login.php
@@ -9,7 +9,25 @@ trueならログイン完了しindex.phpにページ遷移（ID渡す）
 falseならエラー出力
 -->
 <?php
-    $id = htmlspecialchars($_POST["id"]);
-    $pass = htmlspecialchars($_POST["pass"]);
-
+    session_start();
+    $pdo = new PDO('mysql:dbname=bs;host=localhost', 'root', '');
+    $status = "none";
+    if(isset($_SESSION["id"])){
+        $status = "logged_in";
+    }else{
+        $id = htmlspecialchars($_POST["id"]);
+        $pass = htmlspecialchars($_POST["pass"]);
+        var_dump($id);
+        var_dump($pass);
+        $stmt = $pdo->query('SET NAMES utf8');
+        $stmt = $pdo->prepare("SELECT * FROM user WHERE id='$id' AND pass='$pass'");
+        $flag = $stmt->execute();
+        if($flag==false){
+            header("Location: top.php");
+        }else{
+            //セッションにユーザidを保存
+            $_SESSION["id"] = $id;
+            header("Location: index.php");
+        }
+    }
 ?>
