c6e52bbf510d897c713bbcd131936a7ee98f3a4e
中島弘貴 <zero.flowerbox.isk76@gmail.com>
2015-10-01 14:38:23 +0900

    exit追加

--- login.php
+++ login.php
@@ -11,23 +11,18 @@ falseならエラー出力
 <?php
     session_start();
     $pdo = new PDO('mysql:dbname=bs;host=localhost', 'root', '');
-    $status = "none";
-    if(isset($_SESSION["id"])){
-        $status = "logged_in";
+    $id = htmlspecialchars($_POST["id"]);
+    $pass = htmlspecialchars($_POST["pass"]);
+    $stmt = $pdo->query('SET NAMES utf8');
+    $stmt = $pdo->prepare("SELECT * FROM user WHERE id='$id' AND pass='$pass'");
+    $flag = $stmt->execute();
+    if($flag==false){
+        header("Location: top.php");
+        exit;
     }else{
-        $id = htmlspecialchars($_POST["id"]);
-        $pass = htmlspecialchars($_POST["pass"]);
-        var_dump($id);
-        var_dump($pass);
-        $stmt = $pdo->query('SET NAMES utf8');
-        $stmt = $pdo->prepare("SELECT * FROM user WHERE id='$id' AND pass='$pass'");
-        $flag = $stmt->execute();
-        if($flag==false){
-            header("Location: top.php");
-        }else{
-            //セッションにユーザidを保存
-            $_SESSION["id"] = $id;
-            header("Location: index.php");
-        }
+        //セッションにユーザidを保存
+        $_SESSION["id"] = $id;
+        header("Location: index.php");
+        exit;
     }
 ?>
