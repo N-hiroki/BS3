59c7b3c476ad007ccdceb1f601ff8feebe449bf1
中島弘貴 <zero.flowerbox.isk76@gmail.com>
2015-10-10 06:01:17 +0900

    更新

--- bs.sql
+++ bs.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 2015 年 9 月 30 日 08:52
+-- Generation Time: 2015 年 10 月 07 日 05:14
 -- サーバのバージョン： 5.6.26
 -- PHP Version: 5.6.12
 
@@ -44,7 +44,13 @@ INSERT INTO `ans` (`id`, `ans`, `ans_user`, `date`) VALUES
 (15, '', 'ZEROFROM', '2015-09-30 08:24:10'),
 (15, 'aaa', 'aa', '2015-09-30 08:26:06'),
 (11, 'ddfsf', 'aa', '2015-09-30 08:26:36'),
-(16, 'qqq', 'aa', '2015-09-30 08:26:55');
+(16, 'qqq', 'aa', '2015-09-30 08:26:55'),
+(19, '回答', 'aa', '2015-09-30 09:26:12'),
+(20, 'パワポ', 'aa', '2015-10-01 07:35:41'),
+(16, 'sca', 'aa', '2015-10-03 05:05:35'),
+(22, 'aa', 'a', '2015-10-06 10:42:27'),
+(15, 'あ', 'a', '2015-10-06 10:45:53'),
+(24, 'a', 'a', '2015-10-06 10:58:52');
 
 -- --------------------------------------------------------
 
@@ -58,7 +64,7 @@ CREATE TABLE IF NOT EXISTS `question` (
   `question` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `date` datetime NOT NULL,
   `user_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
 
 --
 -- テーブルのデータのダンプ `question`
@@ -82,7 +88,13 @@ INSERT INTO `question` (`id`, `title`, `question`, `date`, `user_id`) VALUES
 (15, 'あああa', 'あああa', '2015-09-30 08:18:50', 'ZEROFROM'),
 (16, 'aaaa', 'aaas', '2015-09-30 08:26:18', 'aa'),
 (17, 'マネジメント', 'マネジメントについて教えて下さい。', '2015-09-30 08:44:52', 'aa'),
-(18, 'トレーニングについて', 'ティーチングとコーチングの使い分けについて教えてください。', '2015-09-30 08:45:32', 'aa');
+(18, 'トレーニングについて', 'ティーチングとコーチングの使い分けについて教えてください。', '2015-09-30 08:45:32', 'aa'),
+(19, 'sss', 'sss', '2015-09-30 09:25:50', 'aa'),
+(20, 'プレゼンについて１２３４', 'プレゼンについてですが、準備はどのようなことをすればいいでしょうか？', '2015-10-01 07:35:28', 'aa'),
+(21, 'aa', 'aa', '2015-10-03 05:05:21', 'aa'),
+(22, 'テスト', 'テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテスト', '2015-10-03 08:30:25', 'a'),
+(23, 'qq', 'qq', '2015-10-06 10:58:25', 'a'),
+(24, 'ww', 'wwa', '2015-10-06 10:58:37', 'a');
 
 -- --------------------------------------------------------
 
@@ -144,7 +156,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `question`
 --
 ALTER TABLE `question`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=19;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=25;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
